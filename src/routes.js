cc8c9163b2f2cc137e4d242c0f6d8c15f92a99b5
hoiyin123hk@gmail.com <yinli@MSI.localdomain>
2022-02-22 18:09:16 +0800

    22/2 updated auth signup login logout profile editprofile

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 import { Router } from 'express'
+import authenticateUser from './_middlewares/authenticate-user.js'
 
 const router = Router()
 
 // STATIC
 router.get('/', (await import('./controllers/static/home.js')).default)
+
+// 3rd party API
 router.get('/api/search', (await import('./controllers/api/search.js')).default)
 router.get('/api/showOne', (await import('./controllers/api/showOne.js')).default)
+// Auth
+router.post('/api/auth/signup', (await import('./controllers/api/auth/signup.js')).default)
+router.post('/api/auth/login', (await import('./controllers/api/auth/login.js')).default)
+router.delete('/api/auth/logout', (await import('./controllers/api/auth/logout.js')).default)
+// API | MY PROFILE | AUTH REQUIRED
+router.get('/api/my/profile', authenticateUser('json'), (await import('./controllers/api/my/profile.js')).default)
+router.put('/api/my/profile', authenticateUser('json'), (await import('./controllers/api/my/editprofile.js')).default)
 
 export default router
