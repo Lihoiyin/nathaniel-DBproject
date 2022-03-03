43e282c8ba234405de20a776c7517d092b07d65c
hoiyin123hk@gmail.com <yinli@MSI.localdomain>
2022-03-03 23:29:07 +0800

    3/3

--- src/controllers/api/books/search.js
+++ src/controllers/api/books/search.js
@@ -1,19 +1,23 @@
 import fetch from 'node-fetch'
 
 const controllersApiSearch = async (req, res) => {
-  let { q1, q2, q3 } = req.params
+  let { q1, q2, q3, q4 } = req.params
   console.log(req.params)
   if (q1 === 'notFound') {
     q1 = ''
   }
   if (q2 === 'notFound') {
     q2 = ''
+  } else {
+    q2 = `+inauthor:${q2}`
   }
   if (q3 === 'notFound') {
     q3 = ''
+  } else {
+    q3 = `+subject:${q3}`
   }
-
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${q1}+subject:${q3}+inauthor:${q2}&printType=books&maxResults=40&key=AIzaSyB5noX_cJTwroPi1fLZ_qyJtI94Ah6qWKU`
+  q4 = Number(q4) === 0 ? 1 : 40 * Number(q4)
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${q1}${q3}${q2}&printType=books&maxResults=40&startIndex=${q4}&key=AIzaSyB5noX_cJTwroPi1fLZ_qyJtI94Ah6qWKU`
   const booksData = await fetch(
     url,
     { method: 'GET' }
